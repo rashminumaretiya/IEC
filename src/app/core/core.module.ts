6e19929310038e47c088982ba9b914145874f008
rashminumaretiya <rashmin.tagline@gmail.com>
2022-01-10 11:48:46 +0530

    Header UI

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     HttpClientModule,
+    NgbDropdownModule
   ],
   exports: [
     HeaderComponent,
